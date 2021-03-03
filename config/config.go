029afbccec7f53b6a199881f045963020976fc3b
Philipp Vincent Ebert <philipp@ebertonline.eu>
2021-03-03 13:21:08 +0100

    configuration

---
+++ config/config.go
@@ -0,0 +1,23 @@
+package config
+
+import (
+	"log"
+	"time"
+
+	"github.com/joeshaw/envdecode"
+)
+
+type Conf struct {
+	Port         int           `env:"SERVER_PORT,required"`
+	TimeoutRead  time.Duration `env:"SERVER_TIMEOUT_READ,required"`
+	TimeoutWrite time.Duration `env:"SERVER_TIMEOUT_WRITE,required"`
+	TimeoutIdle  time.Duration `env:"SERVER_TIMEOUT_IDLE,required"`
+}
+
+func Appconfig() *Conf {
+	var c Conf
+	if err := envdecode.StrictDecode(&c); err != nil {
+		log.Fatalf("Failed to decode: %s", err)
+	}
+	return &c
+}
